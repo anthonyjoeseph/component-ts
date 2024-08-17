0caf508ad5c595ac9db0a0bcbde5c1276df0a751
Anthony Gabriele <anthonyjoeseph@gmail.com>
2024-08-17 01:09:17 -0700

    think it works?

--- src/lib/state/array/diff.ts
+++ src/lib/state/array/diff.ts
@@ -28,9 +28,23 @@ export const arrayDiffOrd = <A>(prev: SortedArray<A>, current: SortedArray<A>, o
   return [];
 };
 
+const findIndexAtOccurrence = <A>(arr: A[], occurrence: number, fn: (val: A) => boolean): number => {
+  let occurCount = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i])) {
+      if (occurCount === occurrence) {
+        return i;
+      } else {
+        occurCount++;
+      }
+    }
+  }
+  return -1;
+};
+
 type KeepEffect = { type: "keep"; from: number; to: number };
 type MoveEffect = { type: "move"; from: number; to: number };
-type ReplaceEffect = { type: "replace"; to: number; count: number };
+type ReplaceEffect = { type: "replace"; from: number; to: number; count: number };
 type DeleteEffect = { type: "delete"; from: number };
 type InsertEffect = { type: "insert"; to: number; count: number };
 type ArrayEffect = KeepEffect | MoveEffect | ReplaceEffect | DeleteEffect | InsertEffect;
@@ -68,16 +82,49 @@ export const arrayDiffEq = <A>(prev: A[], current: A[], eq: Eq.Eq<A> = Eq.eqStri
         };
   });
 
+  let moveSources: number[] = [];
+  let moveDestinations: { prevKeep: number; skipInserts: number }[] = [];
+  const bumpMoveSources = (latestInsert: number, numInserted: number, upperBound?: number) => {
+    moveSources = moveSources.map((source) =>
+      latestInsert < source && (!upperBound || source <= upperBound) ? source + numInserted : source
+    );
+  };
+  const bumpMoveDestinations = (latestInsert: number, numInserted: number, upperBound?: number) => {
+    moveDestinations = moveDestinations.map(({ prevKeep, skipInserts }) => ({
+      prevKeep: latestInsert < prevKeep && (!upperBound || prevKeep <= upperBound) ? prevKeep + numInserted : prevKeep,
+      skipInserts,
+    }));
+  };
+  const bumpMoveDestinationInserts = (keepIndex: number, insertIndex: number) => {
+    moveDestinations = moveDestinations.map(({ prevKeep, skipInserts }) => ({
+      prevKeep,
+      skipInserts: prevKeep === keepIndex && insertIndex <= skipInserts ? skipInserts + 1 : skipInserts,
+    }));
+  };
+
   // convert insert/deletes -> moves
+  //   for each 'insert'
+  //     if an element already exists that's marked for deletion
+  //       instead, mark it as a 'move'
+  let latestKeep = -1;
+  let numInsertsSinceKeep = 0;
   for (let i = 0; i < currentEffects.length; i++) {
     const currentEffect = currentEffects[i];
-    if (currentEffect.type === "insert") {
+    if (currentEffect.type === "keep") {
+      latestKeep++;
+      numInsertsSinceKeep = 0;
+    } else if (currentEffect.type === "insert") {
       const fromIndex = prevEffects.findIndex(
         (val): val is DeleteEffect => val.type === "delete" && eq.equals(prev[val.from], current[i])
       );
       if (fromIndex >= 0) {
-        prevEffects[fromIndex] = { type: "move", from: fromIndex, to: i };
-        currentEffects[i] = { type: "move", from: fromIndex, to: i };
+        const to = findIndexAtOccurrence(prevEffects, latestKeep, (val) => val.type === "keep") + 1;
+        prevEffects[fromIndex] = { type: "move", from: fromIndex, to };
+        currentEffects[i] = { type: "move", from: fromIndex, to };
+        moveSources.push(fromIndex);
+        moveDestinations.push({ prevKeep: to, skipInserts: numInsertsSinceKeep });
+      } else {
+        numInsertsSinceKeep++;
       }
     }
   }
@@ -92,51 +139,169 @@ export const arrayDiffEq = <A>(prev: A[], current: A[], eq: Eq.Eq<A> = Eq.eqStri
         (currentEffects[streakStart] as InsertEffect).count++;
         (currentEffects[i] as InsertEffect).count = 0;
       }
+    } else {
+      streakStart = undefined;
     }
   }
-  currentEffects = currentEffects.filter((val) => !(val.type === "insert" && val.count === 0));
 
   // convert inserts -> replacements
-  currentEffects = currentEffects.map((currentEffect, index): ArrayEffect => {
-    if (
-      0 < index &&
-      index < currentEffects.length - 1 &&
-      currentEffects[index - 1].type === "keep" &&
-      currentEffect.type === "insert" &&
-      currentEffects[index + 1].type === "keep"
-    ) {
-      return {
-        type: "replace",
-        to: currentEffect.to,
-        count: currentEffect.count,
-      };
+  //   if prevEffects contains keep/delete/keep pattern
+  //   find longest insert between corresponding keeps in currentEffects
+  const eligibleDeletes = prevEffects
+    .filter(
+      (prevEffect, i): prevEffect is DeleteEffect =>
+        0 < i &&
+        i < prevEffects.length - 1 &&
+        prevEffects[i - 1].type === "keep" &&
+        prevEffect.type === "delete" &&
+        prevEffects[i + 1].type === "keep"
+    )
+    .map((val) => ({
+      val,
+      prevKeep: prevEffects[val.from - 1] as KeepEffect,
+      nextKeep: prevEffects[val.from + 1] as KeepEffect,
+    }));
+  for (const { val, prevKeep, nextKeep } of eligibleDeletes) {
+    let biggestInsert: { val: InsertEffect; to: number } | undefined;
+    for (let to = prevKeep.to; to < nextKeep.to; to++) {
+      const currentEffect = currentEffects[to];
+      if (currentEffect.type === "insert" && (!biggestInsert || currentEffect.count > biggestInsert.val.count)) {
+        biggestInsert = { val: currentEffect, to };
+      }
     }
-    return currentEffect;
-  });
+    if (biggestInsert) {
+      const replace: ReplaceEffect = {
+        type: "replace",
+        from: val.from,
+        to: biggestInsert.to,
+        count: biggestInsert.val.count,
+      };
+      prevEffects[val.from] = replace;
+      currentEffects[biggestInsert.to] = replace;
+    }
+  }
 
   // build w/ actual elements
+  //    delete, replace, move, insert, prepend, append ?
 
   const domActions: DOMAction<A>[] = [];
+
+  let appendIndex: number | undefined;
+  for (let i = currentEffects.length - 1; i >= 0; i--) {
+    const currentEffect = currentEffects[i];
+    if (currentEffect.type === "keep") break;
+    if (currentEffect.type === "insert" && currentEffect.count > 0) {
+      appendIndex = i;
+      break;
+    }
+  }
+
+  // delete
+
+  // a 'keep segment' is the # indices _before_ a 'keep' element
+  let keepSegmentLength: number[] = [];
+  let currentSegmentLength = 1;
+  for (let i = 0; i < prevEffects.length; i++) {
+    const currentEffect = prevEffects[i];
+    if (currentEffect.type === "delete") {
+      domActions.push({
+        type: "deleteAt",
+        index: currentEffect.from,
+      });
+      bumpMoveSources(i, -1);
+      bumpMoveDestinations(i, -1);
+    }
+    if (prevEffects[i].type === "keep") {
+      keepSegmentLength.push(currentSegmentLength);
+      currentSegmentLength = 1;
+    } else {
+      currentSegmentLength++;
+    }
+  }
+
+  // append / replace / insert / prepend
+  let whichKeep = -1;
+  let latestInsert = 0;
+  for (const currentEffect of currentEffects) {
+    if (currentEffect.type === "keep") {
+      whichKeep++;
+      latestInsert += keepSegmentLength[whichKeep];
+      continue;
+    }
+    if (currentEffect.type === "insert") {
+      if (currentEffect.count === 0) continue;
+      const items = current.slice(currentEffect.to, currentEffect.to + currentEffect.count);
+      if (currentEffect.to === 0) {
+        domActions.push({ type: "prepend", items });
+        bumpMoveSources(0, items.length);
+        bumpMoveDestinations(0, items.length);
+        latestInsert += items.length;
+      } else if (currentEffect.to === appendIndex) {
+        domActions.push({ type: "append", items });
+      } else {
+        domActions.push({ type: "insertAt", items, index: latestInsert });
+        bumpMoveSources(latestInsert, items.length);
+        bumpMoveDestinations(latestInsert, items.length);
+        latestInsert += items.length;
+      }
+    } else if (currentEffect.type === "replace") {
+      const items: A[] = current.slice(currentEffect.to, currentEffect.to + currentEffect.count);
+      domActions.push({ type: "replaceAt", index: latestInsert, items });
+      bumpMoveSources(latestInsert, items.length);
+      bumpMoveDestinations(latestInsert, items.length);
+      latestInsert += items.length;
+    }
+  }
+  // move
+  for (let moveIndex = 0; moveIndex < moveSources.length; moveIndex++) {
+    const { prevKeep, skipInserts } = moveDestinations[moveIndex];
+    const source = moveSources[moveIndex];
+    const destination = prevKeep + skipInserts;
+    domActions.push({ type: "move", source, destination });
+
+    bumpMoveDestinationInserts(prevKeep, skipInserts);
+    if (source < destination) {
+      bumpMoveSources(source, -1, destination);
+      bumpMoveDestinations(source, -1, destination);
+    } else {
+      bumpMoveSources(destination, 1, source);
+      bumpMoveDestinations(destination, 1, source);
+    }
+  }
 
   return domActions as unknown as SafeDOMAction<A>[];
 };
 
-/* const oldArray = [0, 1, 2, 3, 4, 5];
+const testAppend = arrayDiffEq([0, 1, 2], [0, 1, 2, 3]);
 
-const newArray = [99, 1, 2, 4, 5, 7, 3, 9]; 
-*/
+const testPrepend = arrayDiffEq([0, 1, 2], [-1, 0, 1, 2]);
 
-const oldArray = [0, 1, 2];
+const testReplace = arrayDiffEq([0, 1, 2], [0, 10, 11, 12, 2]);
 
-const newArray = [0, 10, 11, 12, 2];
+// [0, 1, 2, 3, 4], [0, 4, 9, 1, 9, 2, 9, 3]
 
-const lcs = arrayDiffEq(oldArray, newArray);
+const testMoveBack = arrayDiffEq([0, 1, 2, 3, 4, 5, 6, 7], [0, 5, 9, 6, 9, 7, 1, 2, 3, 4]);
 
-console.log(lcs);
+const testMoveForward = arrayDiffEq([0, 1, 2, 3, 4, 5, 6, 7], [0, 4, 5, 6, 1, 9, 2, 9, 3, 7]);
+
+console.log(JSON.stringify({ testAppend, testPrepend, testReplace, testMoveBack, testMoveForward }, null, 2));
 
 /**
-prev: keep, move, delete
-current: keep, move, insert, replace
+ * test move forward:
+ * [0,1,2,3,4]
+ * - insert [9] @ &1
+ * [0,9,1,2,3,4]
+ * - insert [9] @ &3
+ * [0,9,1,9,2,3,4]
+ * - insert [9] @ &5
+ * [0,9,1,9,2,9,3,4]
+ * - move &7 to &1
+ * [0,4,9,1,9,2,9,3]
+ */
+
+/**
+prev: keep, move, replace, delete
+current: keep, move, replace, insert
 
 prev:
 {type: 'delete', from: 0}
