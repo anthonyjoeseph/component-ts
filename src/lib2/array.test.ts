3baea4b61025aeb68cd367a3e0d9131a64c076df
Anthony Gabriele <anthonyjoeseph@gmail.com>
2024-12-22 22:04:43 -0500

    first test

--- src/lib2/array.test.ts
+++ src/lib2/array.test.ts
@@ -1,150 +1,46 @@
 import * as r from "rxjs";
-import { StaticAction, element as e, InitAction, ModifyAction, ChildAction, RxNode } from "./element";
+import {
+  StaticAction,
+  element as e,
+  InitAction,
+  ModifyAction,
+  ChildAction,
+  RxNode,
+  DynamicAction,
+  DynamicInitAction,
+} from "./element";
 import { array as a } from "./array";
 import { h } from "hastscript";
 import { describe, test } from "node:test";
 import * as assert from "node:assert/strict";
+import { DOMAction } from "../lib/state/array/domAction";
 
-const filterIds = (actions: StaticAction[]): StaticAction[] =>
+const scrubIdCallbacks = (actions: DynamicAction[]): DynamicAction[] =>
   actions.map((action) =>
-    action.type !== "init"
+    action.type !== "dynamic-init"
       ? action
       : ({
-          type: "init",
-          node: action.node,
-        } as InitAction)
+          type: "dynamic-init",
+          index: action.index,
+          action: {
+            type: "init",
+            node: action.action.node,
+          },
+        } as DynamicInitAction)
   );
 
 describe("array", () => {
-  test.skip("has properties", async () => {
-    const node = e("a", { href: r.of("abcd") });
-    const initAction = await r.firstValueFrom(node);
-    assert.deepEqual(filterIds([initAction]), [
-      { type: "init", node: h("a", { id: "a", href: "abcd" }) } as InitAction,
-    ]);
-  });
-
-  test.skip("delayed properties", async () => {
-    const node = e("a", { href: r.of("abcd").pipe(r.delay(0)) }).pipe(r.toArray());
+  test("insertAt single", async () => {
+    const node = a(
+      r.of({ type: "insertAt", index: 0, items: [e("a", { href: r.of("abcd") })] } as DOMAction<RxNode>)
+    ).pipe(r.toArray());
     const actions = await r.firstValueFrom(node);
-    assert.deepEqual(filterIds(actions), [
-      { type: "init", node: h("a", { id: "a" }) } as InitAction,
-      { type: "modify", id: "a", property: { href: "abcd" } } as ModifyAction,
-    ]);
-  });
-
-  test.skip("has children", async () => {
-    const node = e("div", { hidden: r.of(false) }, [
-      e("a", { href: r.of("abcd") }),
-      e("a", { href: r.of("1234") }),
-      e("a", { href: r.of("!@#$") }),
-    ]);
-    const initAction = await r.firstValueFrom(node);
-    const plainElement = h("div", { id: "div", hidden: false }, [
-      h("a", { id: "div-0a", href: "abcd" }),
-      h("a", { id: "div-1a", href: "1234" }),
-      h("a", { id: "div-2a", href: "!@#$" }),
-    ]);
-    assert.deepEqual(filterIds([initAction]), [
+    assert.deepEqual(scrubIdCallbacks(actions), [
       {
-        type: "init",
-        node: plainElement,
-      } as InitAction,
-    ]);
-  });
-
-  test.skip("most recent sync child is initialized", async () => {
-    const node = e("div", { hidden: r.of(false) }, [
-      e("a", { href: r.of("abcd") }),
-      r.of("0", "1", "2").pipe(r.switchMap((num) => e("a", { href: r.of(num) }))),
-      e("a", { href: r.of("!@#$") }),
-    ]);
-    const actions = await r.firstValueFrom(node);
-    assert.deepEqual(filterIds([actions]), [
-      {
-        type: "init",
-        node: h("div", { id: "div", hidden: false }, [
-          h("a", { id: "div-0a", href: "abcd" }),
-          h("a", { id: "div-3a", href: "2" }),
-          h("a", { id: "div-4a", href: "!@#$" }),
-        ]),
-      } as InitAction,
-    ]);
-  });
-
-  test("delayed children", async () => {
-    const node = e("div", { hidden: r.of(false) }, [
-      e("a", { href: r.of("abcd") }),
-      e("a", { href: r.of("1234") }).pipe(r.delay(0)),
-      e("a", { href: r.of("!@#$") }),
-    ]);
-    const actions = await r.firstValueFrom(node.pipe(r.toArray()));
-    assert.deepEqual(filterIds(actions), [
-      {
-        type: "init",
-        node: h("div", { id: "div", hidden: false }, [
-          h("a", { id: "div-0a", href: "abcd" }),
-          h("a", { id: "div-1a", href: "!@#$" }),
-        ]),
-      } as InitAction,
-      {
-        type: "child",
-        targetId: "div",
-        domAction: { type: "insertAt", index: 1, items: [h("a", { id: "div-2a", href: "1234" })] },
-      } as ChildAction,
-    ]);
-  });
-
-  test.skip("delayed child property", async () => {
-    const node = e("div", { hidden: r.of(false) }, [
-      e("a", { href: r.of("abcd") }),
-      e("a", { href: r.of("1234"), hidden: r.of(false).pipe(r.delay(0)) }),
-      e("a", { href: r.of("!@#$") }),
-    ]);
-    const actions = await r.firstValueFrom(node.pipe(r.toArray()));
-    assert.deepEqual(filterIds(actions), [
-      {
-        type: "init",
-        node: h("div", { id: "div", hidden: false }, [
-          h("a", { id: "div-0a", href: "abcd" }),
-          h("a", { id: "div-1a", href: "1234" }),
-          h("a", { id: "div-2a", href: "!@#$" }),
-        ]),
-      } as InitAction,
-      {
-        type: "modify",
-        id: "div-3a",
-        property: { hidden: false },
-      } as ModifyAction,
-    ]);
-  });
-
-  test.skip("children in series", async () => {
-    const node = e("div", { hidden: r.of(false) }, [
-      e("a", { href: r.of("abcd") }),
-      r.merge(r.of("0"), r.of("1", "2").pipe(r.delay(0))).pipe(r.switchMap((num) => e("a", { href: r.of(num) }))),
-      e("a", { href: r.of("!@#$") }),
-    ]);
-    const actions = await r.firstValueFrom(node.pipe(r.toArray()));
-    assert.deepEqual(filterIds(actions), [
-      {
-        type: "init",
-        node: h("div", { id: "div", hidden: false }, [
-          h("a", { id: "div-0a", href: "abcd" }),
-          h("a", { id: "div-1a", href: "0" }),
-          h("a", { id: "div-2a", href: "!@#$" }),
-        ]),
-      } as InitAction,
-      {
-        type: "child",
-        targetId: "div",
-        domAction: { type: "replaceAt", index: 1, items: [h("a", { id: "div-3a", href: "1" })] },
-      } as ChildAction,
-      {
-        type: "child",
-        targetId: "div",
-        domAction: { type: "replaceAt", index: 1, items: [h("a", { id: "div-4a", href: "2" })] },
-      } as ChildAction,
+        type: "dynamic-init",
+        index: 0,
+        action: { type: "init", node: h("a", { id: "a", href: "abcd" }) },
+      } as DynamicInitAction,
     ]);
   });
 });
--- src/lib2/array.ts
+++ src/lib2/array.ts
@@ -1,55 +1,214 @@
 import * as r from "rxjs";
 import { DOMAction } from "../lib/state/array/domAction";
-import {
-  RxDynamicNode,
-  RxNode,
-  DynamicAction,
-  DynamicInitAction,
-  DynamicModifyAction,
-  DynamicChildAction,
-  DynamicChildAncestorAction,
-  StaticAction,
-  InitAction,
-  ModifyAction,
-  ChildAction,
-} from "./element";
+import { RxDynamicNode, RxNode, DynamicAction, DynamicChildAction, StaticAction } from "./element";
+import { Element } from "hast";
 
-type InternalMemory = { permanentId: number; relativeIndex: number; absoluteIndex: number; finish: r.Subject<void> };
+type InternalMemory = {
+  permanentId: number;
+  relativeIndex: number;
+  absoluteIndex: number;
+  length: number;
+  finish: r.Subject<void>;
+};
+
+const range = (start: number, end: number) => new Array<number>(end - start).fill(0).map((_, index) => start + index);
 
 const insertChildren = (
   index: number,
-  children: RxNode[],
+  numberOfChildren: number,
   memory: InternalMemory[],
   latestPermanentId: number
-): void => {};
+): void => {
+  for (const memoryIndex of range(0, memory.length)) {
+    const entry = memory[memoryIndex];
+    if (entry.relativeIndex >= index) {
+      entry.relativeIndex += numberOfChildren;
+    }
+    if (entry.absoluteIndex >= index) {
+      entry.absoluteIndex += numberOfChildren;
+    }
+  }
+  const newEntries: InternalMemory[] = range(0, numberOfChildren).map((childIndex) => ({
+    permanentId: latestPermanentId + childIndex,
+    relativeIndex: index,
+    absoluteIndex: index + childIndex,
+    length: childIndex === 0 ? numberOfChildren : 1,
+    finish: new r.Subject<void>(),
+  }));
+  memory.splice(index, 0, ...newEntries);
+};
 
-const removeChild = (absoluteIndex: number, memory: InternalMemory[]) => {};
+const removeChild = (absoluteIndex: number, memory: InternalMemory[]) => {
+  for (const memoryIndex of range(0, memory.length)) {
+    const entry = memory[memoryIndex];
+    if (entry.absoluteIndex >= absoluteIndex) {
+      entry.absoluteIndex--;
+    }
+  }
+  memory.splice(absoluteIndex, 1);
+};
 
-const moveChildren = (sourceAbsoluteIndex: number, destinationAbsoluteIndex: number, memory: InternalMemory[]) => {};
-
-const recordChildAction = (
-  action: StaticAction | DynamicAction,
-  permanentId: number,
-  memory: InternalMemory[]
-): void => {};
+const moveChildren = (sourceAbsoluteIndex: number, destinationAbsoluteIndex: number, memory: InternalMemory[]) => {
+  const child = memory[sourceAbsoluteIndex];
+  if (destinationAbsoluteIndex < sourceAbsoluteIndex) {
+    for (const memoryIndex of range(0, memory.length)) {
+      const entry = memory[memoryIndex];
+      if (entry.absoluteIndex >= destinationAbsoluteIndex && entry.absoluteIndex <= sourceAbsoluteIndex) {
+        entry.absoluteIndex++;
+      }
+    }
+    memory.splice(sourceAbsoluteIndex, 1);
+    memory.splice(destinationAbsoluteIndex, 0, child);
+  } else {
+    for (const memoryIndex of range(0, memory.length)) {
+      const entry = memory[memoryIndex];
+      if (entry.absoluteIndex >= sourceAbsoluteIndex && entry.absoluteIndex <= destinationAbsoluteIndex) {
+        entry.absoluteIndex--;
+      }
+    }
+    memory.splice(sourceAbsoluteIndex, 1);
+    memory.splice(destinationAbsoluteIndex - 1, 0, child);
+  }
+};
 
 const convertChildAction = (
   action: StaticAction | DynamicAction,
   permanentId: number,
   memory: InternalMemory[]
-): DynamicAction => {};
+): DynamicAction[] => {
+  const memoryItem = memory.find((im) => im.permanentId === permanentId);
+  const index = memoryItem.absoluteIndex;
+  if (action.type === "modify") {
+    return [{ type: "dynamic-modify", index, action }];
+  } else if (action.type === "init") {
+    return [{ type: "dynamic-init", index, action: action }];
+  } else if (action.type === "child") {
+    return [
+      {
+        type: "dynamic-child-ancestor",
+        targetId: action.targetId,
+        domAction: action.domAction,
+      },
+    ];
+  } else if (action.type === "dynamic-child-ancestor") {
+    return [action];
+  } else if (action.type === "dynamic-modify") {
+    return [{ type: "dynamic-modify", index: index + action.index, action: action.action }];
+  } else if (action.type === "dynamic-init") {
+    return [{ type: "dynamic-init", index: index + action.index, action: action.action }];
+  } else if (action.type === "dynamic-child") {
+    if (action.domAction.type === "replaceAll") {
+      const replaces = range(0, Math.min(memoryItem.length, action.domAction.items.length)).map(
+        (childIndex): DynamicChildAction => ({
+          type: "dynamic-child",
+          domAction: {
+            type: "replaceAt",
+            index: index + childIndex,
+            items: [(action.domAction as { items: Element[] }).items[childIndex]],
+          },
+        })
+      );
+      const deletes =
+        memoryItem.length > action.domAction.items.length
+          ? range(action.domAction.items.length, memoryItem.length).map(
+              (childIndex): DynamicChildAction => ({
+                type: "dynamic-child",
+                domAction: {
+                  type: "deleteAt",
+                  index: childIndex,
+                },
+              })
+            )
+          : [];
+      const inserts: DynamicChildAction[] =
+        memoryItem.length < action.domAction.items.length
+          ? [
+              {
+                type: "dynamic-child",
+                domAction: {
+                  type: "insertAt",
+                  index: memoryItem.length,
+                  items: action.domAction.items.slice(memoryItem.length),
+                },
+              },
+            ]
+          : [];
+      return [...replaces, ...deletes, ...inserts];
+    } else if (action.domAction.type === "prepend") {
+      return [{ type: "dynamic-child", domAction: { type: "insertAt", index, items: action.domAction.items } }];
+    } else if (
+      action.domAction.type === "deleteAt" ||
+      action.domAction.type === "insertAt" ||
+      action.domAction.type === "replaceAt"
+    ) {
+      return [{ type: "dynamic-child", domAction: { ...action.domAction, index: index + action.domAction.index } }];
+    } else if (action.domAction.type === "move") {
+      return [
+        {
+          type: "dynamic-child",
+          domAction: {
+            type: "move",
+            source: index + action.domAction.source,
+            destination: index + action.domAction.destination,
+          },
+        },
+      ];
+    }
+  }
+};
 
+const recordChildAction = (action: DynamicAction, permanentId: number, memory: InternalMemory[]): void => {
+  if (action.type === "dynamic-modify" || action.type === "dynamic-child-ancestor") {
+    // do nothing
+  } else if (action.type === "dynamic-init") {
+    for (const memoryIndex of range(0, memory.length)) {
+      const entry = memory[memoryIndex];
+      if (entry.absoluteIndex >= action.index) {
+        entry.absoluteIndex++;
+        entry.relativeIndex++;
+      }
+    }
+    const newEntry: InternalMemory = {
+      permanentId,
+      relativeIndex: action.index,
+      absoluteIndex: action.index,
+      length: 1,
+      finish: new r.Subject<void>(),
+    };
+    memory.splice(action.index, 0, newEntry);
+  } else if (action.type === "dynamic-child") {
+    if (action.domAction.type === "deleteAt") {
+      removeChild(action.domAction.index, memory);
+    } else if (action.domAction.type === "replaceAt") {
+      removeChild(action.domAction.index, memory);
+      insertChildren(action.domAction.index, action.domAction.items.length, memory, permanentId);
+    } else if (action.domAction.type === "insertAt") {
+      insertChildren(action.domAction.index, action.domAction.items.length, memory, permanentId);
+    } else if (action.domAction.type === "move") {
+      moveChildren(action.domAction.source, action.domAction.destination, memory);
+    } else if (action.domAction.type === "prepend") {
+      insertChildren(0, action.domAction.items.length, memory, permanentId);
+    } else if (action.domAction.type === "replaceAll") {
+      for (const childIndex of range(0, memory.length)) {
+        removeChild(childIndex, memory);
+      }
+      insertChildren(0, action.domAction.items.length, memory, permanentId);
+    }
+  }
+};
+
+// TODO: out of bounds errors
 export const array = (children: r.Observable<DOMAction<RxNode>>): RxDynamicNode => {
   const memory = [] as InternalMemory[];
   let latestPermanentId = 0;
   return children.pipe(
-    r.mergeMap((action) => {
+    r.mergeMap((action): r.Observable<DynamicAction> => {
       if (action.type === "deleteAt" || action.type === "replaceAt") {
         const finisher = memory.find((im) => im.absoluteIndex === action.index).finish;
         finisher.next();
         finisher.complete();
         removeChild(action.index, memory);
-        if (action.type === "deleteAt") return r.EMPTY;
+        if (action.type === "deleteAt") return r.of({ type: "dynamic-child", domAction: action } as DynamicChildAction);
       }
       if (action.type === "replaceAll") {
         memory.forEach((im) => {
@@ -68,7 +227,7 @@ export const array = (children: r.Observable<DOMAction<RxNode>>): RxDynamicNode 
         latestPermanentId += action.items.length;
         insertChildren(
           action.type === "prepend" || action.type === "replaceAll" ? 0 : action.index,
-          action.items,
+          action.items.length,
           memory,
           latestPermanentId
         );
@@ -76,9 +235,12 @@ export const array = (children: r.Observable<DOMAction<RxNode>>): RxDynamicNode 
           ...action.items.map((childActions, childIndex) => {
             const permanentId = initialPermanentId + childIndex;
             return childActions.pipe(
-              r.map((childAction) => {
-                recordChildAction(childAction, permanentId, memory);
-                return convertChildAction(childAction, permanentId, memory);
+              r.mergeMap((childAction) => {
+                const convertedActions = convertChildAction(childAction, permanentId, memory);
+                for (const convertedAction of convertedActions) {
+                  recordChildAction(convertedAction, permanentId, memory);
+                }
+                return r.of(...convertedActions);
               }),
               r.takeUntil(memory.find((im) => im.permanentId === permanentId).finish)
             );
@@ -86,7 +248,7 @@ export const array = (children: r.Observable<DOMAction<RxNode>>): RxDynamicNode 
         );
       } else if (action.type === "move") {
         moveChildren(action.source, action.destination, memory);
-        return r.EMPTY;
+        return r.of({ type: "dynamic-child", domAction: action } as DynamicChildAction);
       }
     })
   );
