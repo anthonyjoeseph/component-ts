0600fbd6ac712cdc35add916a7eef834416f9194
Anthony Gabriele <anthonyjoeseph@gmail.com>
2025-02-02 21:21:37 -0500

    research

--- src/dctp/index.ts
+++ src/dctp/index.ts
@@ -1,7 +1,7 @@
-import type { Observable } from "rxjs";
-import * as r from 'rxjs';
 import type { IO } from 'fp-ts/IO';
 import type { Task }from 'fp-ts/Task';
+import type { Observable } from "rxjs";
+import * as r from 'rxjs';
 
 export type Time = number;
 export type Interval<A> = { low: A; high: A; };
@@ -13,6 +13,38 @@ export type Event<A> = Iterable<Future<A>>;
 export type Future<A> = r.Timestamp<A>
 
 
+type Frame<A, B> = (delta: number, bufferedEvents: { time: number; value: A }[]) => B;
+
+/**
+ * correspondence between: fran sampling ~~ rxjs scheduler
+ * * let's not re-invent the wheel
+ * * when should sampling happen? And how?
+ * 
+ * how can event and behavior be mutually recursive?
+ * * why is this a requirement in the original spec? Can it be stack safe in js?
+ * 
+ * are there leaks in the abstraction of Event<A> => Event<Behavior<B>> ?
+ * * what about behavior inputs? Like mouse position in the original paper?
+ * * * does that necessarily lead to a space-time leak?
+ * * * what does behavior actually mean in this context?
+ * * * are Behaviors ever invoked for something other than sampling? (peek into future, peek into past?) 
+ * 
+ * can event and behavior be created as top-level entities rather than being confined to a function?
+ * * how do we go from observable -> event -> observable?
+ * * same thing with observable -> behavior -> observable?
+ * 
+ * where does "scan" / "accumE" / "accumB" / "integral" belong in all this?
+ * * feels like IO ... except integral I guess ...
+ * 
+ * is this justifiable? What are the advantages to this in an actual frontend setting?
+ * * besides animation, which is dubious anyway
+ * * is simultaneity useful in the frontend?
+ * * do we want Behaviors of dom inserts/deletes? A new node every 5 seconds?
+ * 
+ */
+
+
+
 
 export const eventToObservable = <A, B>(fn: (e: Event<A>) => Event<Behavior<B>>): (o: Observable<Future<A>>) => Observable<Behavior<B>> => (o) => o.pipe(
   r.scan((val) => {
@@ -21,7 +53,7 @@ export const eventToObservable = <A, B>(fn: (e: Event<A>) => Event<Behavior<B>>)
     let isDone = false;
     do {
       const {value, done} = app[Symbol.iterator]().next();
-      isDone = done;
+      isDone = done ?? false;
     } while(!isDone);
     return undefined;
   }),
--- src/dctp/resources.md
+++ src/dctp/resources.md
@@ -14,6 +14,7 @@
 - https://github.com/SodiumFRP/sodium-typescript
 - https://github.com/funkia/hareactive
 - https://github.com/turion/rhine
+- https://hackage.haskell.org/package/pipes-concurrency-1.0.0/docs/Control-Proxy-Concurrent-Tutorial.html
 
 # papers
 
@@ -24,6 +25,8 @@
 - https://github.com/ocharles/papers/blob/master/Efficient%20and%20Compositional%20Higher-Order%20Streams.pdf
 - https://github.com/beerendlauwers/haskell-papers-ereader/blob/master/papers/Practical%20Principled%20FRP%20-%20Forget%20the%20past%2C%20change%20the%20future%2C%20FRPNow!.pdf
 - https://futureofcoding.org/papers/comprehensible-frp/comprehensible-frp.pdf
+- https://www.sciencedirect.com/science/article/pii/S157106611200045X
+- https://www.semanticscholar.org/paper/LTL-types-FRP%3A-linear-time-temporal-logic-as-types%2C-Jeffrey/b201a51586a218c2c32f48d49179f6511df26976
 
 # blog posts
 
@@ -31,6 +34,9 @@
 - https://elm-lang.org/news/farewell-to-frp
 - https://futureofcoding.org/essays/dctp.html
 - https://apfelmus.nfshost.com/blog/2012/09/03-frp-dynamic-event-switching-0-7.html
+- https://ncatlab.org/nlab/show/temporal+logic#Jeffrey12
+- https://www.haskellforall.com/2013/04/pipes-concurrency-100-reactive.html
+- https://markkarpov.com/post/free-monad-considered-harmful.html
 
 # books
 
