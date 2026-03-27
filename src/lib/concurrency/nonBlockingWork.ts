f5fc185b4988e316e161bf2ac041edd410a51504
Anthony Gabriele <anthonyjoeseph@gmail.com>
2026-03-27 11:03:03 -0400

    non blocking work rough draft

--- src/lib/concurrency/nonBlockingWork.ts
+++ src/lib/concurrency/nonBlockingWork.ts
@@ -1,17 +1,14 @@
+export type NonBlockingYield = {
+  type: "yield";
+  nextStepTimeEstimate?: number
+};
 export type NonBlockingDone<A> = {
   type: "done";
   value: A;
 };
-export type NonBlockingEstimate = {
-  type: "estimate";
-  timeNeeded: number;
-};
-export type NonBlockingYield = {
-  type: "yield";
-};
-export type NonBlockingEmission<A> = NonBlockingDone<A> | NonBlockingEstimate | NonBlockingYield;
 
-export type NonBlockingGenerator<A> = Generator<NonBlockingEmission<A>, never, number | undefined>;
+export type NonBlockingGenerator<A> = Generator<NonBlockingYield, NonBlockingDone<A>, number>;
+
 
 /**
  * Generalized function simulating react's "concurrent mode" execution.
@@ -23,6 +20,8 @@ export type NonBlockingGenerator<A> = Generator<NonBlockingEmission<A>, never, n
  * 
  * Alternately, we might try `queueMicrotask`: https://github.com/facebook/react/blob/3cb2c42013eda273ac449126ab9fcc115a09d39d/packages/react/src/ReactAct.js#L361
  * 
+ * I guess requestIdleCallback wasn't as "aggressive" as the react team wanted https://github.com/facebook/react/issues/11171#issuecomment-417349573
+ * 
  * 
  * We use a synchronous generator as a simple-ish interface
  * 
@@ -33,43 +32,89 @@ export type NonBlockingGenerator<A> = Generator<NonBlockingEmission<A>, never, n
  */
 export const nonBlockingWork = <A>(
   generator: NonBlockingGenerator<A>,
-  millisPerCycle = 10,
-  timeout?: number
+  timeout = Number.MAX_VALUE,
+  workPeriodMillis = 10,
 ): Promise<A> => {
-  if (typeof setImmediate === "function") {
-    return new Promise<A>((resolve) => {
-      const workLoop = () => {
-        const start = new Date().getTime();
-        while () {
-
-        }
-        const { value: yielded } = generator.next(undefined);
-        if (yielded.type === "done") {
-          resolve(yielded.value);
-        } else {
-          setImmediate(workLoop);
-        }
-      };
-      workLoop();
-    });
-  } else if (typeof MessageChannel !== undefined) {
-  } else if (window !== undefined && typeof window?.requestIdleCallback === "function") {
-    return new Promise<A>((resolve) => {
-      const workLoop: IdleRequestCallback = ({ timeRemaining, didTimeout }) => {
-        while () {
-          const { value: yielded } = generator.next(undefined);
-          if (yielded.type === "done") {
-            resolve(yielded.value);
-          } else if (yielded.type === "estimate") {
-            window.requestIdleCallback(workLoop, { timeout });
-          } else {
-            window.requestIdleCallback(workLoop, { timeout });
-          }
-        }
-      };
-      const start = new Date().getTime();
-      workLoop({ timeRemaining: () => millisPerCycle - (new Date().getTime() - start), didTimeout: undefined as any });
-    });
+  const postpone = (work: () => void) => {
+    if (typeof setImmediate === "function") {
+      setImmediate(work);
+    } else if (typeof MessageChannel !== "undefined") {
+      const channel = new MessageChannel();
+      const port = channel.port2;
+      channel.port1.onmessage = work;
+      port.postMessage(null);
+    } else {
+      setTimeout(work, 0);
+    }
   }
-  return new Promise((resolve) => {});
+
+  return new Promise<A>((resolve) => {
+    const startTime = new Date().getTime();
+    const workLoop = () => {
+      const periodStartTime = new Date().getTime();
+      let currentTime = periodStartTime;
+      let resolved = false;
+      while (currentTime - periodStartTime < workPeriodMillis || timeout < (currentTime - startTime)) {
+        const { value: yielded } = generator.next(workPeriodMillis - (currentTime - periodStartTime));
+        if (yielded.type === "done") {
+          resolved = true;
+          resolve(yielded.value);
+          break
+        } else if (
+          yielded.nextStepTimeEstimate !== undefined &&
+          yielded.nextStepTimeEstimate > (currentTime - periodStartTime) &&
+          (currentTime - startTime) <= timeout
+        ) {
+          break;
+        }
+        currentTime = new Date().getTime();
+      }
+      if (!resolved) {
+        postpone(workLoop);
+      }
+    };
+    postpone(workLoop);
+  });
+};
+
+export const nonBlockingIdleWork =  <A>(
+  generator: NonBlockingGenerator<A>,
+  timeout?: number,
+  workPeriodMillis?: number,
+): Promise<A> => {
+  if (window !== undefined && typeof window?.requestIdleCallback === "function") {
+    return new Promise<A>((resolve) => {
+      const startTime = new Date().getTime();
+      const workLoop: IdleRequestCallback = ({ timeRemaining: getTimeRemaining, didTimeout }) => {
+        let timeRemaining: number = getTimeRemaining();
+        let resolved = false;
+        while (timeRemaining > 0 || didTimeout) {
+          const { value: yielded } = generator.next(timeRemaining);
+          if (yielded.type === "done") {
+            resolved = true;
+            resolve(yielded.value);
+            break;
+          } else if (
+            yielded.nextStepTimeEstimate !== undefined && 
+            yielded.nextStepTimeEstimate > timeRemaining && 
+            !didTimeout
+          ) {
+            break;
+          }
+          timeRemaining = getTimeRemaining();
+        }
+        if (!resolved) {
+          window.requestIdleCallback(
+            workLoop,
+            { 
+              timeout: timeout ? timeout - (new Date().getTime() - startTime) : undefined 
+            }
+          );
+        }
+      };
+      window.requestIdleCallback(workLoop, { timeout });
+    });
+  } else {
+    return nonBlockingWork(generator, timeout, workPeriodMillis);
+  }
 };
